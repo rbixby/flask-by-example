3d282f35ea3baf338f559acab3d2b0f98c2d61ca
Roger Wood Bixby <1766203+rbixby@users.noreply.github.com>
2019-06-04 23:04:42 -0700

    Added Alembic and SQLAlchemy support.

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'change_me'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):

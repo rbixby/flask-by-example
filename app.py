b9627bf4d546767bc4e8779f577b06c43d3e58c2
Roger Wood Bixby <1766203+rbixby@users.noreply.github.com>
2019-06-06 08:31:31 -0700

    incremental checkin

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+import requests
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,9 +11,19 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # Get the entered URL
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append("Unable to get URL. Please make sure it is valid and try again.")
+    return render_template('index.html', errors=errors, results=results)
 
 
 @app.route("/<name>")

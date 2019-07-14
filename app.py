4e9bf6bed2fffc1eeeefe84a86323ad286540fc2
Roger Wood Bixby <1766203+rbixby@users.noreply.github.com>
2019-07-14 11:20:52 -0700

    Updated to count words.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 import requests
+import operator
+import re
+import nltk
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -21,8 +28,43 @@ def index():
             url = request.form['url']
             r = requests.get(url)
             print(r.text)
-        except:
+        except Exception as e:
             errors.append("Unable to get URL. Please make sure it is valid and try again.")
+            print(e)
+            return render_template('index.html', errors=errors, results=results)
+        if r:
+            # Text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('.nltk_data/')    # Set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_words_count = Counter(raw_words)
+
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_words_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except Exception as ex:
+                errors.append("Unable to add item to database.")
+                print(ex)
     return render_template('index.html', errors=errors, results=results)
 
 
